f71c882b438bbbb3da36ce471fcebf34db2f45d5
Yoshiyuki Kinjo <yskkin@gmail.com>
2014-08-12 22:59:45 +0900

    Implement assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cached version of matrix which efficiently calculates its inverse
+## when result is required multiple times.
 
-## Write a short comment describing this function
+## Create cached version of matrix.
+## This is represented by a list with following 4 functions:
+##  - set : Set the underlying matrix.
+##  - get : Get the underlying matrix.
+##  - setInverse : Set an inverse of the underlying matrix.
+##  - getInverse : Get an inverse of the underlying matrix.
+##
+## Args:
+##  x: A underlying matrix represented by the result.
+##
+## Returns:
+##  A list which can be regarded as a cached version of matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
 
+    set <- function(mat) {
+        x <<- mat？
+        # We have to erase answer since it is no longer right.
+        inverse <<- NULL
+    }
+    get <- function() {
+        x
+    }
+
+    # This function is intended to be used only by `cacheSolve`.
+    # Calling this directly may result in wrong answer.
+    setInverse <- function(inv) {
+        inverse <<- inv
+    }
+    getInverse <- function() {
+        inverse
+    }
+
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Calculate an inverse matrix.
+## This function uses answer if available from previous call
+## for saving time.
+##
+## Args:
+##  x: Cached version of matrix created by `makeCacheMatrix`.
+##
+## Returns:
+##  An inverse matrix of x, non-cached version.  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if (!is.null(inverse)) {
+        message("getting cached data")
+        inverse
+    } else {
+        inverse <- solve(x$get(), ...)
+        x$setInverse(inverse)
+        inverse
+    }
 }
+
+# Below is just for testing.
+
+test <- makeCacheMatrix(matrix(1:4, 2, 2))
+print(test$get())
+print(test$getInverse())
+testInverse <- cacheSolve(test)
+testInverse <- cacheSolve(test)
+
+print(test$get() %*% testInverse)
